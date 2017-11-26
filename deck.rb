813771e48e4ac88c084774e5dc5da7cfdd9e1e98
danilpanshin <danilpanshin@gmail.com>
2017-11-26 19:56:34 +0300

    aces sum don't work

--- deck.rb
+++ deck.rb
@@ -3,10 +3,11 @@ class Deck
   attr_accessor :user_cards, :dealer_cards, :cards, :user_sum, :dealer_sum
 
   def initialize
-    @cards=["2+", "3+", "4+", "5+", "6+", "7+", "8+", "9+", "10+", "K+", "Q+", "J+", "A+",
-            "2<3", "3<3", "4<3", "5<3", "6<3", "7<3", "8<3", "9<3", "10<3", "K<3", "Q<3", "J<3", "A<3",
-            "2<>", "3<>", "4<>", "5<>", "6<>", "7<>", "8<>", "9<>", "10<>", "K<>", "Q<>", "J<>", "A<>",
-            "2^", "3^", "4^", "5^", "6^", "7^", "8^", "9^", "10^", "K^", "Q^", "J^", "A^"]
+    #@cards=["2+", "3+", "4+", "5+", "6+", "7+", "8+", "9+", "10+", "K+", "Q+", "J+", "A+",
+      #      "2<3", "3<3", "4<3", "5<3", "6<3", "7<3", "8<3", "9<3", "10<3", "K<3", "Q<3", "J<3", "A<3",
+       #     "2<>", "3<>", "4<>", "5<>", "6<>", "7<>", "8<>", "9<>", "10<>", "K<>", "Q<>", "J<>", "A<>",
+        #    "2^", "3^", "4^", "5^", "6^", "7^", "8^", "9^", "10^", "K^", "Q^", "J^", "A^"]
+    @cards = ["A+", "A^", "A<3", "A<>" ]
     @cards.shuffle!
     give_out_cards
   end
@@ -35,16 +36,26 @@ class Deck
   def user_points
     sum = 0
     pic_sum = 0
-    numbers = @user_cards.select  { |x| x =~ /[0-9]/ }
+    ace_sum = 0
+    @user_sum = 0
+        numbers = @user_cards.select  { |x| x =~ /[0-9]/ }
     numbers.map! {|x| x.to_i}
     numbers.each {|x| sum += x }
-    pictures = @user_cards.select { |x| x =~ /[JQKA]/ }
+    pictures = @user_cards.select { |x| x =~ /[JQK]/ }
     pictures.each {|x| pic_sum += 10}
+    aces = @user_cards.select {|x| x =~ /[A]/}
     @user_sum = pic_sum + sum
-
-
-
+    if @user_sum < 10
+      aces.each { |x|  ace_sum += 11 }
+      @user_sum = pic_sum + sum + ace_sum
+    else
+      aces.each { |x|  ace_sum += 1 }
     end
+
+
+    @user_sum = pic_sum + sum + ace_sum
+
+    
   end
 
   def dealer_points

d1d7f185a9a6babd2edf3f7870974795dad2220d
danilpanshin <danilpanshin@gmail.com>
2017-11-27 00:23:11 +0300

    aces sum second ver

--- deck.rb
+++ deck.rb
@@ -1,13 +1,13 @@
 class Deck
 
-  attr_accessor :user_cards, :dealer_cards, :cards, :user_sum, :dealer_sum, :aces
+  attr_accessor :user_cards, :dealer_cards, :cards, :user_sum, :dealer_sum, :aces, :pictures, :pic_sum, :ace_sum
 
   def initialize
-    #@cards=["2+", "3+", "4+", "5+", "6+", "7+", "8+", "9+", "10+", "K+", "Q+", "J+", "A+",
-      #      "2<3", "3<3", "4<3", "5<3", "6<3", "7<3", "8<3", "9<3", "10<3", "K<3", "Q<3", "J<3", "A<3",
-       #     "2<>", "3<>", "4<>", "5<>", "6<>", "7<>", "8<>", "9<>", "10<>", "K<>", "Q<>", "J<>", "A<>",
-        #    "2^", "3^", "4^", "5^", "6^", "7^", "8^", "9^", "10^", "K^", "Q^", "J^", "A^"]
-    @cards = ["A+", "A^", "A<3", "A<>",  "A#", "A$", "J+"]
+    @cards=["2+", "3+", "4+", "5+", "6+", "7+", "8+", "9+", "10+", "K+", "Q+", "J+", "A+",
+            "2<3", "3<3", "4<3", "5<3", "6<3", "7<3", "8<3", "9<3", "10<3", "K<3", "Q<3", "J<3", "A<3",
+            "2<>", "3<>", "4<>", "5<>", "6<>", "7<>", "8<>", "9<>", "10<>", "K<>", "Q<>", "J<>", "A<>",
+            "2^", "3^", "4^", "5^", "6^", "7^", "8^", "9^", "10^", "K^", "Q^", "J^", "A^"]
+   # @cards = ["A+", "A^", "A<3", "A<>",  "K#", "Q$", "J+"]
     @cards.shuffle!
     give_out_cards
   end
@@ -35,31 +35,33 @@ class Deck
 
   def user_points
     sum = 0
-    pic_sum = 0
-    ace_sum = 0
+    @pic_sum = 0
+    @ace_sum = 0
     @user_sum = 0
-        numbers = @user_cards.select  { |x| x =~ /[0-9]/ }
+    numbers = @user_cards.select  { |x| x =~ /[0-9]/ }
     numbers.map! {|x| x.to_i}
     numbers.each {|x| sum += x }
-    pictures = @user_cards.select { |x| x =~ /[JQK]/ }
-    pictures.each {|x| pic_sum += 10}
+    @pictures = @user_cards.select { |x| x =~ /[JQK]/ }
+    @pictures.each {|x| @pic_sum += 10}
+
+    @user_sum = @pic_sum + sum
+
     @aces = @user_cards.select {|x| x =~ /[A]/}
-    @user_sum = pic_sum + sum
 
-    if @aces.length == 2
-      ace_sum = 12
+    if aces.empty?
+      @user_sum = @pic_sum + sum
+    elsif @aces.length == 2 || @user_sum <= 9
+      @ace_sum = 12
     elsif @aces.length == 3
-      ace_sum = 13
+      @ace_sum = 13
     elsif @user_sum <= 10 || aces.length == 1
-      ace_sum = 11
+      @ace_sum = 11
     elsif @user_sum > 10 || aces.length == 1
-      ace_sum = 1
-
-
+      @ace_sum = 1
     end
 
 
-    @user_sum = pic_sum + sum + ace_sum
+    @user_sum += @ace_sum
 
 
   end
